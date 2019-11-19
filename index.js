f2509c0126ad70acaaa2ba3cbefc3f3b782aa7b8
Craig Donavin <crdonavin@gmail.com>
2019-11-19 18:02:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const tutorials = [
 const titleCased = () => {
     return tutorials.map(function(string) {
       let words = string.split(' ')
-      const fart = (words => word[0].toUpperCase + word.slice(1))
+      let fart = (words => word[0].toUpperCase + word.slice(1))
       return fart.join(' ')
   })
 }
